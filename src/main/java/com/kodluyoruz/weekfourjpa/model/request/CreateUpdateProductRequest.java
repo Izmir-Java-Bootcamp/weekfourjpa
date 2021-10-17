a94d19a46842596db610dfdf98172775296f6ef7
merthaskan <merthaskan@outlook.com>
2021-10-17 11:06:46 +0300

    lombok

--- src/main/java/com/kodluyoruz/weekfourjpa/model/request/CreateUpdateProductRequest.java
+++ src/main/java/com/kodluyoruz/weekfourjpa/model/request/CreateUpdateProductRequest.java
@@ -1,43 +1,16 @@
 package com.kodluyoruz.weekfourjpa.model.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class CreateUpdateProductRequest {
     private String name;
     private String description;
     private Double price;
-
-    public CreateUpdateProductRequest(String name, String description, Double price) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
-
-    public CreateUpdateProductRequest() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
